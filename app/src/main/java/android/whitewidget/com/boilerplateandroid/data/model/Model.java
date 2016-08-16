ed43b3e1ef43286f1d7e0f8b2cb971cd4523e8b4
Jeh <jehguiao@gmail.com>
2016-08-16 19:02:39 +0800

    Implemented Basicgrid and Basicrow for Recyclerview

--- app/src/main/java/android/whitewidget/com/boilerplateandroid/data/model/Model.java
+++ app/src/main/java/android/whitewidget/com/boilerplateandroid/data/model/Model.java
@@ -7,4 +7,37 @@ public class Model {
     private String id;
     private String title;
     private String subtitle;
+    private String image;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
 }
